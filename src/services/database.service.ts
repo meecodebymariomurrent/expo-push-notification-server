08891c5319a7bb6068522a1e5bbd2f93ca250816
Mario Murrent <mario@murrent.at>
2022-01-22 11:45:32 +0100

    feat: improved connection management

--- src/services/database.service.ts
+++ src/services/database.service.ts
@@ -7,9 +7,13 @@ import * as _ from 'lodash';
 @injectable()
 export class DatabaseService {
 
-    public initialize(): Promise<boolean> {
-        return new Promise<boolean>((resolve, reject) => {
-            this.connect().then((connection: Connection) => {
+    private connection: Connection;
+
+    public async initialize(): Promise<boolean> {
+        try {
+            this.connection = await this.connect();
+            return new Promise<boolean>((resolve, reject) => {
+
                 r.dbList()
                     .contains(this.getDatabaseName())
                     .do((containsDatabase: RDatum<boolean>) => {
@@ -19,9 +23,9 @@ export class DatabaseService {
                             r.dbCreate(this.getDatabaseName())
                         );
                     })
-                    .run(connection)
+                    .run(this.connection)
                     .then(() => {
-                        this.createTables(connection)
+                        this.createTables(this.connection)
                             .then(() => {
                                 resolve(true);
                             })
@@ -30,168 +34,198 @@ export class DatabaseService {
                                 reject(false);
                             });
                     });
-            }).catch((error) => {
-                logger.error('Error while connecting to the database');
-                reject('An error occurred while initializing the database');
             });
-        });
+        } catch (error) {
+            logger.error('Error while connecting to the database', error);
+            return Promise.reject('An error occurred while initializing the database');
+        }
     }
 
-    public connect(): Promise<Connection | boolean> {
-        return new Promise((resolve, reject) => {
-            r.connect(this.getDatabaseConfiguration()).then((connection: Connection) => {
-                resolve(connection);
-            }).catch(() => {
-                reject(false);
+    public connect(): Promise<Connection> {
+        if (this.connection?.open) {
+            return Promise.resolve(this.connection);
+        } else {
+            return new Promise((resolve, reject) => {
+                r.connect(this.getDatabaseConfiguration())
+                    .then((connection: Connection) => {
+                        resolve(connection);
+                    })
+                    .catch(() => {
+                        reject(false);
+                    });
             });
-        });
+        }
     }
 
     public disconnect(connection: Connection): Promise<boolean> {
         return new Promise((resolve, reject) => {
             if (connection) {
-                connection.close().then(() => {
-                    resolve(true);
-                }).catch((error) => {
-                    reject(error);
-                });
+                connection.close()
+                    .then(() => {
+                        resolve(true);
+                    })
+                    .catch((error) => {
+                        reject(error);
+                    });
             }
         });
     }
 
-    public getById<T>(id: string, table: string): Promise<T> {
-        return new Promise((resolve, reject) => {
-            this.connect().then((connection: Connection) => {
+    public async getById<T>(id: string, table: string): Promise<T> {
+        try {
+            this.connection = await this.connect();
+            return new Promise((resolve, reject) => {
                 r.db(this.getDatabaseName())
                     .table(table)
                     .get(id)
-                    .run(connection).then((response: T) => {
-                    resolve(response);
-                }).catch((error) => {
-                    logger.error(error);
-                    reject();
-                });
-            }).catch((error) => {
-                logger.error('Error while obtaining connection');
-                reject(error);
+                    .run(this.connection)
+                    .then((response: T) => {
+                        resolve(response);
+                    })
+                    .catch((error) => {
+                        logger.error(error);
+                        reject();
+                    });
             });
-        });
+        } catch (error) {
+            logger.error('Error while connecting to the database', error);
+            return Promise.reject('An error occurred while initializing the database');
+        }
     }
 
-    public filterBy<T>(filter: any, table: string): Promise<Array<T>> {
-        return new Promise((resolve, reject) => {
-            this.connect().then((connection: Connection) => {
+    public async filterBy<T>(filter: unknown, table: string): Promise<Array<T>> {
+        try {
+            this.connection = await this.connect();
+            return new Promise((resolve, reject) => {
                 r.db(this.getDatabaseName())
                     .table(table)
                     .filter(filter)
-                    .run(connection).then((response: Array<T>) => {
-                    resolve(response);
-                }).catch((error) => {
-                    logger.error(error);
-                    reject();
-                });
-            }).catch((error) => {
-                logger.error('Error while obtaining connection');
-                reject(error);
+                    .run(this.connection)
+                    .then((response: Array<T>) => {
+                        resolve(response);
+                    })
+                    .catch((error) => {
+                        logger.error(error);
+                        reject();
+                    });
             });
-        });
+        } catch (error) {
+            logger.error('Error while connecting to the database', error);
+            return Promise.reject('An error occurred while initializing the database');
+        }
     }
 
-    public filterByIds<T>(ids: Array<string>, property: string, table: string): Promise<Array<T>> {
-        return new Promise((resolve, reject) => {
-            this.connect().then((connection: Connection) => {
+    public async filterByIds<T>(ids: Array<string>, property: string, table: string): Promise<Array<T>> {
+        try {
+            this.connection = await this.connect();
+            return new Promise((resolve, reject) => {
                 r.db(this.getDatabaseName())
                     .table(table)
                     .filter(entry => r.expr(ids).contains(entry(property)))
-                    .run(connection).then((response: Array<T>) => {
-                    resolve(response);
-                }).catch((error) => {
-                    logger.error(error);
-                    reject();
-                });
-            }).catch((error) => {
-                logger.error('Error while obtaining connection');
-                reject(error);
-            });
-        });
-    }
-
-    public getAll<T>(databaseTable: string): Promise<Array<T>> {
-        return new Promise<Array<T>>((resolve, reject) => {
-            this.connect().then((connection: Connection) => {
-                r.db(this.getDatabaseName())
-                    .table(databaseTable)
-                    .filter({})
-                    .run(connection)
+                    .run(this.connection)
                     .then((response: Array<T>) => {
                         resolve(response);
                     }).catch((error) => {
-                    logger.error(`Error while getting all from table ${databaseTable}`, error);
-                });
-            }).catch((error) => {
-                logger.error('Error while obtaining connection');
-                reject(error);
-            });
-        });
-    }
-
-    public add<T>(entity: T, databaseTable: string): Promise<T> {
-        return new Promise((resolve, reject) => {
-            this.connect().then((connection: Connection) => {
-                r.db(this.getDatabaseName())
-                    .table(databaseTable)
-                    .insert(entity)
-                    .run(connection).then(() => {
-                    resolve(entity);
-                }).catch((error) => {
                     logger.error(error);
                     reject();
                 });
-            }).catch((error) => {
-                logger.error('Error while obtaining connection');
-                reject(error);
             });
-        });
+        } catch (error) {
+            logger.error('Error while connecting to the database', error);
+            return Promise.reject('An error occurred while initializing the database');
+        }
     }
 
-    public update<T>(id: string, entity: T, databaseTable: string): Promise<T> {
-        return new Promise((resolve, reject) => {
-            this.connect().then((connection: Connection) => {
+    public async getAll<T>(databaseTable: string): Promise<Array<T>> {
+        try {
+            this.connection = await this.connect();
+            return new Promise<Array<T>>((resolve, reject) => {
+                r.db(this.getDatabaseName())
+                    .table(databaseTable)
+                    .filter({})
+                    .run(this.connection)
+                    .then((response: Array<T>) => {
+                        resolve(response);
+                    })
+                    .catch((error) => {
+                        logger.error(`Error while getting all from table ${databaseTable}`, error);
+                        reject(error);
+                    });
+            });
+        } catch (error) {
+            logger.error('Error while connecting to the database', error);
+            return Promise.reject('An error occurred while initializing the database');
+        }
+    }
+
+    public async add<T>(entity: T, databaseTable: string): Promise<T> {
+        try {
+            this.connection = await this.connect();
+            return new Promise((resolve, reject) => {
+                r.db(this.getDatabaseName())
+                    .table(databaseTable)
+                    .insert(entity)
+                    .run(this.connection)
+                    .then(() => {
+                        resolve(entity);
+                    })
+                    .catch((error) => {
+                        logger.error(error);
+                        reject();
+                    });
+            });
+        } catch (error) {
+            logger.error('Error while connecting to the database', error);
+            return Promise.reject('An error occurred while initializing the database');
+        }
+    }
+
+    public async update<T>(id: string, entity: T, databaseTable: string): Promise<T> {
+        try {
+            this.connection = await this.connect();
+            return new Promise((resolve, reject) => {
                 r.db(this.getDatabaseName())
                     .table(databaseTable)
                     .filter({id})
                     .update(entity)
-                    .run(connection).then(() => {
-                    resolve(entity);
-                }).catch((error) => {
-                    logger.error(error);
-                    reject();
-                });
-            }).catch((error) => {
-                logger.error('Error while obtaining connection');
-                reject(error);
+                    .run(this.connection)
+                    .then(() => {
+                        resolve(entity);
+                    })
+                    .catch((error) => {
+                        logger.error(error);
+                        reject();
+                    });
             });
-        });
+        } catch (error) {
+            logger.error('Error while connecting to the database', error);
+            return Promise.reject('An error occurred while initializing the database');
+        }
     }
 
-    public deleteById(id: string, databaseTable: string): Promise<boolean> {
-        return new Promise((resolve, reject) => {
-            this.connect().then((connection: Connection) => {
-                r.db(this.getDatabaseName())
-                    .table(databaseTable)
-                    .filter({id})
-                    .delete()
-                    .run(connection).then(() => {
-                    resolve(true);
-                }).catch((error) => {
-                    logger.error(error);
-                    reject();
+    public async deleteById(id: string, databaseTable: string): Promise<boolean> {
+        try {
+            this.connection = await this.connect();
+            return new Promise((resolve, reject) => {
+                this.connect().then((connection: Connection) => {
+                    r.db(this.getDatabaseName())
+                        .table(databaseTable)
+                        .filter({id})
+                        .delete()
+                        .run(connection)
+                        .then(() => {
+                            resolve(true);
+                        })
+                        .catch((error) => {
+                            logger.error(error);
+                            reject();
+                        });
                 });
-            }).catch((error) => {
-                logger.error('Error while obtaining connection');
-                reject(error);
             });
-        });
+        } catch (error) {
+            logger.error('Error while connecting to the database', error);
+            return Promise.reject('An error occurred while initializing the database');
+        }
     }
 
     public getFirstEntry<T>(items: Array<T>): T {
