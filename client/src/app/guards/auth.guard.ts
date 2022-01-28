2274653a1af8050cbbdfc53923668b47023d63c3
Mario Murrent <mario@murrent.at>
2022-01-28 08:02:45 +0100

    fix: logout handling for route guard

--- client/src/app/guards/auth.guard.ts
+++ client/src/app/guards/auth.guard.ts
@@ -1,34 +1,36 @@
 import { Injectable } from '@angular/core';
 import {
-    ActivatedRouteSnapshot,
-    RouterStateSnapshot,
-    UrlTree,
-    CanActivate,
-    Router,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  CanActivate,
+  Router,
 } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthenticationService } from '../services/authentication.service';
+import { Page } from '../constants/page.enum';
 
 @Injectable({
-    providedIn: 'root',
+  providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-    constructor(private authenticationService: AuthenticationService,
-                private router: Router) {
-    }
+  constructor(private authenticationService: AuthenticationService,
+              private router: Router) {
+  }
 
-    canActivate(
-        route: ActivatedRouteSnapshot,
-        state: RouterStateSnapshot,
-    ):
-        | Observable<boolean | UrlTree>
-        | Promise<boolean | UrlTree>
-        | boolean
-        | UrlTree {
-        if (this.authenticationService.isLogged()) {
-            return true;
-        }
-        this.authenticationService.logout();
-        return false;
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot,
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    if (this.authenticationService.isLogged()) {
+      return true;
     }
+    this.authenticationService.logout();
+    this.router.navigate([Page.Login]);
+    return false;
+  }
 }
--- client/src/app/services/http/crud.service.ts
+++ client/src/app/services/http/crud.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpErrorResponse, HttpHeaders, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import { HttpHeaderName } from '../../constants/http-header-name.enum';
 import { HttpHeaderValue } from '../../constants/http-header-value.enum';
